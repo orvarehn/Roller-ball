802d21ec391968cb648fb6d9a7958977337f1009
Orvar <orvar.ehn@jayway.com>
2016-10-13 00:34:30 +0200

    Jump button

--- Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -88,7 +88,7 @@ namespace UnityStandardAssets.CrossPlatformInput
 				//delta = Mathf.Clamp(delta, -MovementRange, MovementRange);
 				newPos.y = delta;
 			}
-			transform.position = Vector3.ClampMagnitude (new Vector3 (newPos.x, newPos.y, newPos.z), MovementRange) + m_StartPos;
+			transform.position = Vector3.ClampMagnitude(new Vector3(newPos.x, newPos.y, newPos.z), MovementRange) + m_StartPos;
 			UpdateVirtualAxes(transform.position);
 		}
 
